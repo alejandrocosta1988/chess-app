feed5cb03ebc172df84fe2fe950f2b3d538ab2ec
Alejandro Costa <alejandroesfc@gmail.com>
2022-07-02 07:19:15 -0300

    defaults pawns to be white pieces

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,12 +1,19 @@
 
 public class Pawn {
 
+	private final String WHITE = "white";
+	private final String BLACK = "black";
+
 	private String color;
+	
+	public Pawn() {
+		this.color = WHITE;
+	}
 	
 	public Pawn(String color) {
 		this.color = color;
 	}
-	
+
 	public String getColor() {
 		return color;
 	}
--- src/PawnTest.java
+++ src/PawnTest.java
@@ -6,11 +6,13 @@ public class PawnTest extends junit.framework.TestCase {
 		final String white = "white";
 		final String black = "black";
 		
-		Pawn whitePawn = new Pawn(white);
-		Pawn blackPawn = new Pawn(black);
+		Pawn whitePawn01 = new Pawn(white);
+		Pawn blackPawn01 = new Pawn(black);
+		Pawn whitePawn02 = new Pawn();
 		
-		assertEquals(white, whitePawn.getColor());
-		assertEquals(black, blackPawn.getColor());
+		assertEquals(white, whitePawn01.getColor());
+		assertEquals(black, blackPawn01.getColor());
+		assertEquals(white, whitePawn02.getColor());
 		
 	}
 }
