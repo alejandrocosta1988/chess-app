96d988438be18dd2ad620ac7abe171e13e82c0a3
Alejandro Costa <alejandroesfc@gmail.com>
2022-07-02 08:01:11 -0300

    adds pawns to the board

--- src/chess/board/Board.java
+++ src/chess/board/Board.java
@@ -1,9 +1,24 @@
 package chess.board;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import chess.pieces.Pawn;
+
 public class Board {
+	
+	private ArrayList<Pawn> pieces = new ArrayList<>();
 
 	public Object getNumberOfPieces() {
-		return 0;
+		return pieces.size();
+	}
+
+	public void addPawn(Pawn whitePawn01) {
+		pieces.add(whitePawn01);
+	}
+
+	public ArrayList<Pawn> getPieces() {
+		return pieces;
 	}
 
 }
--- src/chess/board/BoardTest.java
+++ src/chess/board/BoardTest.java
@@ -1,8 +1,13 @@
 package chess.board;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
+
+import chess.pieces.Pawn;
 
 public class BoardTest {
 
@@ -13,6 +18,25 @@ public class BoardTest {
 		
 		assertEquals(0, board.getNumberOfPieces());
 
+	}
+	
+	@Test
+	public void testAddPawnToBoard() {
+		
+		final String BLACK = "black";
+		Board board = new Board();
+		ArrayList<Pawn> pieces = board.getPieces();
+		Pawn whitePawn01 = new Pawn();
+		Pawn blackPawn01 = new Pawn(BLACK);
+		
+		board.addPawn(whitePawn01);
+		assertEquals(1, board.getNumberOfPieces());
+		assertTrue(pieces.contains(whitePawn01));
+		
+		board.addPawn(blackPawn01);
+		assertEquals(2, board.getNumberOfPieces());
+		assertTrue(pieces.contains(blackPawn01));
+		
 	}
 
 }
