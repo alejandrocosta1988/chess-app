653219c1499daedec8cbfa39e68688ff632ecbb1
Alejandro Costa <alejandroesfc@gmail.com>
2022-07-05 12:27:00 -0300

    reorders methods in file for better understanding. Adds .toUpperCase() to setting of black piece printable representation. Changes const piece name to lowercase

--- src/chess/pieces/Piece.java
+++ src/chess/pieces/Piece.java
@@ -8,7 +8,7 @@ public class Piece {
 
 	public static final String WHITE = "white";
 	public static final String BLACK = "black";
-	public static final String PAWN	= "Pawn";
+	public static final String PAWN	= "pawn";
 	
 	private char printableRepresentation;
 
@@ -21,6 +21,14 @@ public class Piece {
 		setPrintableRepresentation(name);
 	}
 	
+	private void setPrintableRepresentation(String pieceName) {
+		if (this.color.equals(BLACK)) {
+			this.printableRepresentation = pieceName.toUpperCase().charAt(0);
+			return;
+		} 
+		this.printableRepresentation = pieceName.toLowerCase().charAt(0);
+	}
+	
 	public static Piece createWhitePawn() {
 		return new Piece(WHITE, PAWN);
 	}
@@ -29,13 +37,6 @@ public class Piece {
 		return new Piece(BLACK, PAWN);
 	}
 	
-	private void setPrintableRepresentation(String pieceName) {
-		if (this.color.equals(BLACK)) {
-			this.printableRepresentation = pieceName.charAt(0);
-			return;
-		} 
-		this.printableRepresentation = pieceName.toLowerCase().charAt(0);
-	}
 
 	public String getColor() {
 		return color;
