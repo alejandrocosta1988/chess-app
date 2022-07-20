232a26317cec8f216a4991c59a1d901a63729a47
Alejandro Costa <alejandroesfc@gmail.com>
2022-07-20 14:20:14 -0300

    implements bishop subtype

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -2,4 +2,26 @@ package chess.pieces;
 
 public class Bishop extends Piece {
 
+	private Bishop(Piece.Color color) {
+		super(color);
+		setPrintableRepresentation('b');
+		setScore(3d);
+	}
+	
+	public static Piece createBlackBishop() {
+		return new Bishop(Piece.Color.BLACK);
+	}
+
+	public static Piece createWhiteBishop() {
+		return new Bishop(Piece.Color.WHITE);
+	}
+	
+	private void setPrintableRepresentation(char representation) {
+		printableRepresentation = representation;
+	}
+	
+	private void setScore(double score) {
+		this.score = score;
+	}
+
 }
--- test/chess/pieces/BishopTest.java
+++ test/chess/pieces/BishopTest.java
@@ -8,68 +8,57 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 class BishopTest {
 
 	private Piece blackBishop;
 	private Piece whiteBishop;
 	
-	@Disabled
 	@BeforeEach
 	void createBlackAndWhiteQueens() {
 		blackBishop = Bishop.createBlackBishop();
 		whiteBishop = Bishop.createWhiteBishop();
 	}
 	
-	@Disabled
 	@Test
 	void testGivenABlackBishopGetColorReturnsBlackColor() {
 		assertTrue(blackBishop.getColor() == Piece.Color.BLACK);
 	}
 	
-	@Disabled
 	@Test
 	void testGivenAWhiteBishopGetColorReturnsWhiteColor() {
 		assertTrue(whiteBishop.getColor() == Piece.Color.WHITE);
 	}
 	
-	@Disabled
 	@Test
 	void testGivenABlackBishopIsBlackReturnsTrue() {
 		assertTrue(blackBishop.isBlack());
 	}
 	
-	@Disabled
 	@Test
 	void testGivenABlackBishopIsWhiteReturnsFalse() {
 		assertFalse(blackBishop.isWhite());
 	}
 	
-	@Disabled
 	@Test
 	void testGivenAWhiteBishopIsWhiteReturnsTrue() {
 		assertTrue(whiteBishop.isWhite());
 	}
 	
-	@Disabled
 	@Test
 	void testGivenAWhiteBishopIsBlackReturnsFalse() {
 		assertFalse(whiteBishop.isBlack());
 	}
 	
-	@Disabled
 	@Test
 	void testABlackBishopShouldBeRepresentedWithAnUpperCaseBBetweenSpaces() {
 		assertEquals(" B ", blackBishop.getRepresentation());
 	}
 	
-	@Disabled
 	@Test
 	void testAWhiteBishopShouldBeRepresentedWithALowerCaseBBetweenSpaces() {
 		assertEquals(" b ", whiteBishop.getRepresentation());
 	}
 	
-	@Disabled
 	@Test
 	void testABishopScoresNine() {
 		assertEquals(3d, whiteBishop.getScore());
