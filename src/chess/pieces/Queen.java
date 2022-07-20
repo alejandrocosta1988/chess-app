40e3daf92cd1ac5001b88ff599a5089aac4de1d7
Alejandro Costa <alejandroesfc@gmail.com>
2022-07-20 18:11:31 -0300

    checks the movements for subtype queen

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -1,5 +1,7 @@
 package chess.pieces;
 
+import chess.moves.IntLocations;
+
 public class Queen extends Piece {
 	
 	private Queen(Piece.Color color) {
@@ -14,6 +16,56 @@ public class Queen extends Piece {
 	
 	public static Piece createWhiteQueen() {
 		return new Queen(Piece.Color.WHITE);
+	}
+	
+	public boolean checkMove(IntLocations locations) {
+		if (isSameFile(locations) || isSameRank(locations)) {
+			return true;
+		}
+		if (isSameDifference(locations)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isSameFile(IntLocations locations) {
+		if (locations.getDestinationFile() == locations.getOriginFile()) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isSameRank(IntLocations locations) {
+		if (locations.getDestinationRank() == locations.getOriginRank()) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isSameDifference(IntLocations locations) {
+		return getDifferenceInFile(locations) == getDifferenceInRank(locations);
+	}
+	
+	private int getDifferenceInFile(IntLocations locations) {
+		if (isMoveTowardsRight(locations)) {
+			return locations.getDestinationFile() - locations.getOriginFile();
+		}
+		return locations.getOriginFile() - locations.getDestinationFile();
+	}
+	
+	private boolean isMoveTowardsRight(IntLocations locations) {
+		return locations.getDestinationFile() > locations.getOriginFile();
+	}
+	
+	private int getDifferenceInRank(IntLocations locations) {
+		if (isMoveForwards(locations)) {
+			return locations.getDestinationRank() - locations.getOriginRank();
+		}
+		return locations.getOriginRank() - locations.getDestinationRank();
+	}
+	
+	private boolean isMoveForwards(IntLocations locations) {
+		return locations.getDestinationRank() > locations.getOriginRank();
 	}
 	
 }
--- test/chess/pieces/QueenTest.java
+++ test/chess/pieces/QueenTest.java
@@ -1,9 +1,13 @@
 package chess.pieces;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import chess.moves.IntLocations;
 
 class QueenTest extends PieceTest {
 
@@ -46,10 +50,76 @@ class QueenTest extends PieceTest {
 		assertEquals(9d, whiteQueen.getScore());
 	}
 
+	@Test
+	void testWhenAQueenTriesToMoveForwardsInStraightLineThenCheckMoveReturnsTrue() {
+		IntLocations locations = new IntLocations("d3", "d7");
+		assertTrue(whiteQueen.checkMove(locations));
+	}
 	
-
+	@Test
+	void testWhenAQueenTriesToMoveBackwardsInStraightLineThenCheckMoveReturnsTrue() {
+		IntLocations locations = new IntLocations("d3", "d1");
+		assertTrue(whiteQueen.checkMove(locations));
+	}
 	
-
+	@Test
+	void testWhenAQueenTriesToMoveTowardsRightInStraightLineThenCheckMoveReturnsTrue() {
+		IntLocations locations = new IntLocations("d3", "g3");
+		assertTrue(whiteQueen.checkMove(locations));
+	}
 	
+	@Test
+	void testWhenAQueenTriesToMoveTowardsLeftInStraightLineThenCheckMoveReturnsTrue() {
+		IntLocations locations = new IntLocations("d3", "a3");
+		assertTrue(whiteQueen.checkMove(locations));
+	}
+	
+	@Test
+	void testWhenAQueenTriesToMoveForwardsAndToTheRightInStraightLineThenCheckMoveReturnsTrue() {
+		IntLocations locations = new IntLocations("d3", "h7");
+		assertTrue(whiteQueen.checkMove(locations));
+	}
+	
+	@Test
+	void testWhenAQueenTriesToMoveForwardsAndToTheLeftInStraightLineThenCheckMoveReturnsTrue() {
+		IntLocations locations = new IntLocations("g2", "b7");
+		assertTrue(whiteQueen.checkMove(locations));
+	}
+	
+	@Test
+	void testWhenAQueenTriesToMoveBackwardsAndToTheLeftInStraightLineThenCheckMoveReturnsTrue() {
+		IntLocations locations = new IntLocations("g7", "b2");
+		assertTrue(whiteQueen.checkMove(locations));
+	}
+	
+	@Test
+	void testWhenAQueenTriesToMoveBackwardsAndToTheRightInStraightLineThenCheckMoveReturnsTrue() {
+		IntLocations locations = new IntLocations("b7", "g2");
+		assertTrue(whiteQueen.checkMove(locations));
+	}
+	
+	@Test
+	void testWhenAQueenTriesToMoveBackwardsAndToTheRightNotInStraightLineThenCheckMoveReturnsFalse() {
+		IntLocations locations = new IntLocations("b7", "g3");
+		assertFalse(whiteQueen.checkMove(locations));
+	}
+	
+	@Test
+	void testWhenAQueenTriesToMoveBackwardsAndToTheLeftNotInStraightLineThenCheckMoveReturnsFalse() {
+		IntLocations locations = new IntLocations("g7", "b4");
+		assertFalse(whiteQueen.checkMove(locations));
+	}
+	
+	@Test
+	void testWhenAQueenTriesToMoveForwardsAndToTheLeftNotInStraightLineThenCheckMoveReturnsFalse() {
+		IntLocations locations = new IntLocations("f1", "b3");
+		assertFalse(whiteQueen.checkMove(locations));
+	}
+	
+	@Test
+	void testWhenAQueenTriesToMoveForwardsAndToTheRightNotInStraightLineThenCheckMoveReturnsFalse() {
+		IntLocations locations = new IntLocations("d1", "h3");
+		assertFalse(whiteQueen.checkMove(locations));
+	}
 	
 }
