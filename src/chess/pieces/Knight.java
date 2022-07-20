764be5b14992ac478ac7275611f264e57da51b98
Alejandro Costa <alejandroesfc@gmail.com>
2022-07-20 14:30:11 -0300

    implements knight subtype

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -2,4 +2,18 @@ package chess.pieces;
 
 public class Knight extends Piece {
 
+	private Knight(Piece.Color color) {
+		super(color);
+		setPrintableRepresentation('n');
+		setScore(2.5);
+	}
+	
+	public static Piece createBlackKnight() {
+		return new Knight(Piece.Color.BLACK);
+	}
+
+	public static Piece createWhiteKnight() {
+		return new Knight(Piece.Color.WHITE);
+	}
+
 }
--- test/chess/pieces/KnightTest.java
+++ test/chess/pieces/KnightTest.java
@@ -5,71 +5,59 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 class KnightTest {
 
 	private Piece blackKnight;
 	private Piece whiteKnight;
 	
-	@Disabled
 	@BeforeEach
 	void createBlackAndWhiteKnights() {
 		blackKnight = Knight.createBlackKnight();
 		whiteKnight = Knight.createWhiteKnight();
 	}
 	
-	@Disabled
 	@Test
 	void testGivenABlackKnightGetColorReturnsBlackColor() {
 		assertTrue(blackKnight.getColor() == Piece.Color.BLACK);
 	}
 	
-	@Disabled
 	@Test
 	void testGivenAWhiteKnightGetColorReturnsWhiteColor() {
 		assertTrue(whiteKnight.getColor() == Piece.Color.WHITE);
 	}
 	
-	@Disabled
 	@Test
 	void testGivenABlackKnightIsBlackReturnsTrue() {
 		assertTrue(blackKnight.isBlack());
 	}
 	
-	@Disabled
 	@Test
 	void testGivenABlackKnightIsWhiteReturnsFalse() {
 		assertFalse(blackKnight.isWhite());
 	}
 	
-	@Disabled
 	@Test
 	void testGivenAWhiteKnightIsWhiteReturnsTrue() {
 		assertTrue(whiteKnight.isWhite());
 	}
 	
-	@Disabled
 	@Test
 	void testGivenAWhiteKnightIsBlackReturnsFalse() {
 		assertFalse(whiteKnight.isBlack());
 	}
 	
-	@Disabled
 	@Test
 	void testABlackKnightShouldBeRepresentedWithAnUpperCaseNBetweenSpaces() {
 		assertEquals(" N ", blackKnight.getRepresentation());
 	}
 	
-	@Disabled
 	@Test
 	void testAWhiteKnightShouldBeRepresentedWithALowerCaseNBetweenSpaces() {
 		assertEquals(" n ", whiteKnight.getRepresentation());
 	}
 	
-	@Disabled
 	@Test
 	void testAKnightScoresTwoAndAHalf() {
 		assertEquals(2.5, whiteKnight.getScore());
